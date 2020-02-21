78593b1f739827fb19a2cd6ddc0e67d123999045
Nishanthreddy1997 <46684557+Nishanthreddy1997@users.noreply.github.com>
2020-02-21 16:02:30 -0600

    Updated comments

--- data/Nishanth/1mapper.py
+++ data/Nishanth/1mapper.py
@@ -4,6 +4,7 @@ output = open("01.txt", "w")
 for line in input:
     datalist = line.strip().split("\t")
     date, location, mintemp, maxtemp, rainfall, winddirection, windspeed, humidity, presssure, tempat9, Raintoday  = datalist
+   # mintemp of each locaton
     output.write(location + "\t" + mintemp + "\n")
 
 input.close()
